ecaf94b60795c4d4923779be3a1128122916aeef
Pedro Henrique <henriquepedrorosamoura@gmail.com>
2025-09-10 01:53:54 -0300

    atualizaçao meus dados e excluir perfil

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -1,9 +1,14 @@
-from django.contrib import admin # Adicione essa linha
 from django.urls import path, include
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('usuario/', include('usuario.urls')),
+    path('cadastrar/', views.cadastrar_usuario, name='cadastrar'),
+    
+    # Adicione estas URLs para as páginas de perfil
+    path('meus_dados/', views.meus_dados, name='meus_dados'),
+    path('meus_dados/editar/', views.editar_dados, name='editar_dados'),
+    path('meus_dados/excluir/', views.excluir_perfil, name='excluir_perfil'),
 ]
--- djangoapp/views.py
+++ djangoapp/views.py
@@ -1,5 +1,36 @@
-# djangoapp/views.py
 from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def cadastrar_usuario(request):
+    return render(request, 'cadastrar.html')
+
+def meus_dados(request):
+    usuario = {
+        'nome': 'Henrique',
+        'email': 'henrique@email.com',
+        'ra': '101100101',
+        'turma': 'pro (manhã)',
+        'telefone': '11986941564',
+    }
+    return render(request, 'meus_dados.html', {'usuario': usuario})
+
+def editar_dados(request):
+    usuario = {
+        'nome': 'Henrique',
+        'email': 'henrique@email.com',
+        'ra': '101100101',
+        'turma': 'pro (manhã)',
+        'telefone': '11986941564',
+    }
+    return render(request, 'editar_dados.html', {'usuario': usuario})
+
+def excluir_perfil(request):
+    usuario = {
+        'nome': 'Henrique',
+    }
+    return render(request, 'excluir_perfil.html', {'usuario': usuario})
